3ddef189b5acd9067d92b55735f02814d535e94f
Fabio Goffin <fabio.goffin@gmail.com>
2023-03-22 20:29:57 +0100

    login update

--- login.js
+++ login.js
@@ -1,32 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+import { createUserWithEmailAndPassword, getAuth} from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
+import { ref, getDatabase, set } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js";
 
-// Usa body-parser per analizzare i dati POST del form
-app.use(bodyParser.urlencoded({ extended: true }));
+const firebaseConfig = {
+  apiKey: "AIzaSyCcIwVONO6_7Who1aiaIg6MAYTORo-jCOA",
+  authDomain: "provasitodb-5851f.firebaseapp.com",
+  databaseURL: "https://provasitodb-5851f-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "provasitodb-5851f",
+  storageBucket: "provasitodb-5851f.appspot.com",
+  messagingSenderId: "129381431270",
+  appId: "1:129381431270:web:f6f851f64f5fb09c558827"
+};
 
-// Gestisce la richiesta POST dal form
-app.post('/login', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  
-  // Verifica le credenziali dell'utente dal database
-  if (checkCredentials(username, password)) {
-    // Credenziali valide, mostra la pagina di benvenuto
-    res.send('Benvenuto, ' + username);
-  } else {
-    // Credenziali non valide, mostra un messaggio di errore
-    res.send('Credenziali non valide');
-  }
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+var auth = getAuth(app);
+const db = getDatabase();
+
+
+const register = document.getElementById("register");
+register.addEventListener('click', function(){
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(function(){
+      const now = new Date();
+      const hours = now.getHours();
+      const minutes = now.getMinutes().toString().padStart(2, '0');
+      const day = now.getDate();
+      const month = (now.getMonth() + 1).toString().padStart(2, '0'); 
+      const year = now.getFullYear();
+
+      const formattedDate = `${hours}:${minutes} ${day}/${month}/${year}`;
+      let user = auth.currentUser;
+      set(ref(db, 'users/' + user.uid), {
+        email : email,
+        password : password,
+        last_login : formattedDate
+      });
+      alert("done!");
+    })
+    .catch(function(error){
+      alert(error.message);
+    });
 });
-
-// Avvia il server su una porta specifica
-app.listen(3000, () => {
-  console.log('Server in ascolto sulla porta 3000');
-});
-
-// Funzione per verificare le credenziali dell'utente dal database
-function checkCredentials(username, password) {
-  // Effettua una query al database per verificare le credenziali dell'utente
-  // Restituisce true se le credenziali sono valide, altrimenti false
-}
